bbc4c701a4a37fa31fb219935057898c9220fabe
RicardoRonan <ricardoronanm@gmail.com>
2022-08-11 09:50:52 +0200

    first commit

---
+++ routes/albumsRoute.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+const middleware = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+
+// Get all albums
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM albums", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+      console.log(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Gets one album
+router.get("/:id", (req, res) => {
+  try {
+    con.query(
+      `Select * from albums where album_id=${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+// Add album
+router.post("/", middleware, (req, res) => {
+  console.log("added new album successfully");
+  if (req.user.user_type === "admin") {
+    const {
+      user_id,
+      album_name,
+      artists,
+      price,
+      descriptions,
+      image,
+      release_date,
+      quantity,
+      tracklist,
+    } = req.body;
+    try {
+      con.query(
+        `INSERT into albums (user_id,album_name,artists,price,descriptions,image,release_date,quantity,tracklist) values ('${user_id}' ,'${album_name}', '${artists}', '${price}', '${descriptions}' , '${image}' , '   ${release_date}' , '${quantity}','${tracklist}')`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+// edit album
+router.put("/:id", middleware, (req, res) => {
+  if (req.user.user_type === "admin") {
+    console.log("updated album successfully");
+    const {
+      user_id,
+      album_name,
+      artists,
+      price,
+      descriptions,
+      image,
+      release_date,
+      quantity,
+      tracklist,
+    } = req.body;
+    try {
+      con.query(
+        `UPDATE albums SET user_id="${user_id}",album_name="${album_name}", artists="${artists}", price="${price}",descriptions="${descriptions}",image="${image}",release_date="${release_date}",quantity="${quantity}",tracklist="${tracklist}" WHERE album_id="${req.params.id}"`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+// delete albums
+router.delete("/:id", middleware, (req, res) => {
+  if (req.user.user_type === "admin") {
+    console.log("deleted album successfully");
+    try {
+      con.query(
+        `DELETE FROM albums  WHERE album_id="${req.params.id}"`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+
+module.exports = router;
---
+++ routes/userRoute.js
@@ -0,0 +1,203 @@
+const nodemailer = require("nodemailer");
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+const middleware = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const authController = require("../controller/Auth/index");
+// get all
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Gets one users
+router.get("/:id", middleware, (req, res) => {
+  try {
+    con.query(
+      `SELECT * FROM users where user_id =${req.params.id} `,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// edit user
+router.put("/:id", (req, res) => {
+  const { email, password, full_name, address, country, phone, user_type } =
+    req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  try {
+    con.query(
+      `UPDATE users SET email="${email}",password="${hash}",full_name="${full_name}",address="${address}",country="${country}",phone="${phone}",user_type="${user_type}" WHERE users.user_id="${req.params.id}"`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+// delete user
+router.delete("/:id", (req, res) => {
+  try {
+    con.query(
+      `DELETE FROM users  WHERE users.user_id="${req.params.id}"`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Register Route and Add new user
+// The Route where Encryption starts
+router.post("/register", (req, res) => {
+  return authController.Register(req, res);
+});
+
+// Login
+router.post("/login", (req, res) => {
+  return authController.Login(req, res);
+});
+
+// Verify
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  return authController.Verify(req, res);
+});
+// Importing the dependencies
+
+router.post("/forgot-psw", (req, res) => {
+  try {
+    let sql = "SELECT * FROM users WHERE ?";
+    let user = {
+      email: req.body.email,
+    };
+    console.log(user);
+    con.query(sql, user, (err, result) => {
+      if (err) throw err;
+      if (result === 0) {
+        res.status(400), res.send("Email not found");
+      } else {
+        // Allows me to connect to the given email account || Your Email
+        const transporter = nodemailer.createTransport({
+          host: process.env.MAILERHOST,
+          port: process.env.MAILERPORT,
+          auth: {
+            user: process.env.MAILERUSER,
+            pass: process.env.MAILERPASS,
+          },
+        });
+        console.log(transporter);
+        // How the email should be sent out
+        var mailData = {
+          from: process.env.MAILERUSER,
+          // Sending to the person who requested
+          to: result[0].email,
+
+          subject: "Password Reset",
+          html: `<div>
+            <h3>Hi ${result[0].full_name},</h3>
+            <br>
+            <h4>Click link below to reset your password</h4>
+
+            <a href="/resetpsw.html">
+              Click Here to Reset Password
+              user_id = ${result[0].user_id}
+            </a>
+
+            <br>
+            <p>For any queries feel free to contact us...</p>
+            <div>
+              Email: ${process.env.MAILERUSER}
+              <br>
+              Tel: If needed you can add this
+            <div>
+          </div>`,
+        };
+
+        // Check if email can be sent
+        // Check password and email given in .env file
+        transporter.verify((error, success) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email valid! ", success);
+          }
+        });
+
+        transporter.sendMail(mailData, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.status(200).json({
+              status: "ok",
+              data: result,
+            });
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Rest Password Route
+
+router.put("reset-psw/:id", (req, res) => {
+  let sql = "SELECT * FROM users WHERE ?";
+  let user = {
+    user_id: req.params.id,
+  };
+  con.query(sql, user, (err, result) => {
+    if (err) throw err;
+    if (result === 0) {
+      res.status(400), res.send("User not found");
+    } else {
+      let newPassword = `UPDATE users SET ? WHERE user_id = ${req.params.id}`;
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+
+      const updatedPassword = {
+        full_name: result[0].full_name,
+        email: result[0].email,
+        user_type: result[0].user_type,
+        phone: result[0].phone,
+        country: result[0].country,
+        address: result[0].address,
+        default_shipping_address: result[0].default_shipping_address,
+
+        // Only thing im changing in table
+        password: hash,
+      };
+
+      con.query(newPassword, updatedPassword, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Password Updated please login");
+      });
+    }
+  });
+});
+
+module.exports = router;
