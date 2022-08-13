808b39159e4e5f6d74026ef6ad3b48dbb5e54cf3
Sabelo Mdashe <selbysabelomdashe@gmail.com>
2022-08-13 14:51:04 +0200

    was working to get one product

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -34,15 +34,24 @@ function showalbums(albums) {
 
 // Getting One Album
 
-function getOneAlbum(id) {
-  id = albums.album_id;
-  fetch(`https://coleworld.herokuapp.com/albums/?id=${albums.album_id}`)
-    .then((res) => res.json())
-    .then((data) => {
-      albums = data;
-      console.log(data);
-      document.write(`<h1>${albums.album_name}</h1>`);
-    });
-  console.log(id);
+async function getOneAlbum(id) {
+  const res = await fetch(`https://coleworld.herokuapp.com/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  const data = await res.json();
+  albums = data;
   console.log(albums);
+  showAlbum(albums);
+}
+
+async function showAlbum(albumArray) {
+  console.log(albumArray);
+  const show = await albumArray.forEach((album) => {
+    document.querySelector(
+      ".albums_div"
+    ).innerHTML = `<h1>${album.album_name}</h1>`;
+  });
 }
